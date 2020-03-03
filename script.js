d657ae99f7163c607ac01dd422ac0a5d63a0e2c2
Romina Villaverde <romina.villaverde@gmail.com>
2020-03-02 20:25:28 -0500

    drawer styling, some global changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function collapse() {
-  return false;
+function collapse(e) {
+  let target = $(e.target);
+  let parent = target.closest('.collapsable-wrapper');
+  parent.find('.collapsable').slideToggle();
 }
 
 $(function() {
@@ -7,11 +9,76 @@ $(function() {
   const header = $('.mdc-header');
   const appBar = $('.mdc-top-app-bar');
 
-  window.onscroll = fixed;
-  fixed();
+  if (drawer) {
+    $(window).scroll(function() {
+      onScroll();
+    });
+
+    onScroll();
+  }
+
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(window).off("scroll");
+      
+      $('.mdc-list-item').removeClass('mdc-list-item--activated');
+      $(this).addClass('mdc-list-item--activated');
+    
+      var target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top-appBar.height() - 12
+      }, 500, 'swing', function () {
+          $(window).scroll("scroll", onScroll);
+          //window.location.hash = target;
+      });
+  });
+
+  function onScroll() {
+    fixed();
+    updateDrawer();
+  }
+
+  function updateDrawer() {
+    const scrollTop = $(this).scrollTop();
+    
+    let sections = $('.main-wrapper section');
+    let sectionId;
+
+    let threshold = header.height();
+
+    if (scrollTop < threshold) {
+      return;
+    }
+
+    sections.each(function(i, section) {
+      section = $(section);
+      if (scrollTop > section.position().top && section.position().top + section.height() - scrollTop >= 0) {
+        sectionId = section.attr('id');
+        console.log(section.attr('id'));
+      } 
+/*      console.log($(section).height());
+      console.log($(section).offset().top - scrollTop);*/
+      //if ($(section).position().top > 0) activeSection = section;
+/*      if ($(section).offset().top < 0 && $(section).height() + scrollTop > 0) {
+        console.log(section);
+
+      }*/
+    });
+
+    $('.mdc-list-item').each(function(i, item) {
+      if ($(item).attr('data-section') == sectionId) {
+        console.log($(item).attr('data-section'));
+        $(item).addClass('mdc-list-item--activated');
+      } else {
+        $(item).removeClass('mdc-list-item--activated');
+      }
+    });
+  }
 
   function fixed() {
-    const scrollTop = $(document).scrollTop();
+    const scrollTop = $(this).scrollTop();
     if (header.height() > scrollTop) {
       drawer.removeClass('fixed');
       drawer.css('top', `${ header.height() }px`);
@@ -19,5 +86,7 @@ $(function() {
       drawer.addClass('fixed');
       drawer.css('top', `${ appBar.height() }px`);
     }
-  }
+  };
+
+  $('.collapse-link').click(collapse);
 });
--- theme.js
+++ theme.js
@@ -1,13 +1,15 @@
-var buttons = document.querySelectorAll('.mdc-button, .mdc-fab');
+var buttons = $('.mdc-button, .mdc-fab');
 for (var i = 0, button; button = buttons[i]; i++) {
   mdc.ripple.MDCRipple.attachTo(button);
 }
 
-var textFields = document.querySelectorAll('.mdc-text-field');
+var textFields = $('.mdc-text-field');
 for (var i = 0, textField; textField = textFields[i]; i++) {
   mdc.textField.MDCTextField.attachTo(textField);
 }
-
-const list = mdc.list.MDCList.attachTo(document.querySelector('.mdc-list'));
-
-//const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+/*
+const lists = mdc.list.MDCList.attachTo($('.mdc-list'));
+for (var i = 0, textField; textField = textFields[i]; i++) {
+  mdc.textField.MDCTextField.attachTo(textField);
+}
+*/
