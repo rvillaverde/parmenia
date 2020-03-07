ae8572475f62d10388f124b345cfee12fd5a34a9
Romina Villaverde <romina.villaverde@gmail.com>
2020-03-07 15:00:42 -0500

    empty project page

--- theme.js
+++ theme.js
@@ -7,6 +7,11 @@ var textFields = $('.mdc-text-field');
 for (var i = 0, textField; textField = textFields[i]; i++) {
   mdc.textField.MDCTextField.attachTo(textField);
 }
+
+var tabScrollers = $('.mdc-tab-scroller');
+for (var i = 0, tabScroller; tabScroller = tabScrollers[i]; i++) {
+  mdc.tabScroller.MDCTabScroller.attachTo(tabScroller);
+}
 /*
 const lists = mdc.list.MDCList.attachTo($('.mdc-list'));
 for (var i = 0, textField; textField = textFields[i]; i++) {
