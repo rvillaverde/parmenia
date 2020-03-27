7bae1c309144fab8a49d928d4d1301c89a05e6b3
Romina Villaverde <romina.villaverde@gmail.com>
2020-03-27 14:42:52 -0500

    user roles, some minor changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,95 @@
-let dialog;
+let dialog, drawer, header, appBar;
+
+$(function() {
+  drawer = $('.mdc-drawer');
+  header = $('.mdc-header');
+  appBar = $('.mdc-top-app-bar');
+
+  if (drawer) {
+    $(window).scroll(function() {
+      onScroll();
+    });
+
+    onScroll();
+  }
+
+  $('.user-roles-wrapper input').change(roleSelection);
+  roleSelection({ currentTarget: $('.user-roles-wrapper input[checked]') });
+
+  //smoothscroll
+  $('.mdc-drawer a[href^="#"]').on('click', smoothScroll);
+
+  $('.cards--selectable .mdc-card').click(function(e) {
+    let card = $(e.currentTarget);
+    card.closest('.cards--selectable').find('.mdc-card').removeClass('mdc-card--selected');
+    setoggleCardSelection(card);
+  });
+
+  $('.draggable').mousedown(onGrab);
+
+  $('.collapse-link').click(collapse);
+});
+
+function smoothScroll(e) {
+  e.preventDefault();
+  $(window).off("scroll");
+  
+  $('.mdc-list-item').removeClass('mdc-list-item--activated');
+  $(this).addClass('mdc-list-item--activated');
+
+  var target = this.hash;
+  $target = $(target);
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top-appBar.height() - 12
+  }, 500, 'swing', function () {
+    $(window).scroll("scroll", onScroll);
+    //window.location.hash = target;
+  });
+}
+
+function onScroll() {
+  fixed();
+  updateDrawer();
+}
+
+function updateDrawer() {
+  const scrollTop = $(this).scrollTop();
+  
+  let sections = $('.main-wrapper section');
+  let sectionId;
+
+  let threshold = header.height();
+
+  if (scrollTop < threshold) {
+    return;
+  }
+
+  sections.each(function(i, section) {
+    section = $(section);
+    if (scrollTop > section.position().top && section.position().top + section.height() - scrollTop >= 0) {
+      sectionId = section.attr('id');
+    } 
+  });
+
+  $('.mdc-list-item').each(function(i, item) {
+    if ($(item).attr('data-section') == sectionId) {
+      $(item).addClass('mdc-list-item--activated');
+    } else {
+      $(item).removeClass('mdc-list-item--activated');
+    }
+  });
+}
+
+function fixed() {
+  const scrollTop = $(this).scrollTop();
+  if (header.height() > scrollTop) {
+    drawer.removeClass('fixed');
+    drawer.css('top', `${ header.height() }px`);
+  } else {
+    drawer.addClass('fixed');
+    drawer.css('top', `${ appBar.height() }px`);
+  }
+};
 
 function collapse(e) {
   let target = $(e.target);
@@ -13,10 +104,11 @@ function collapse(e) {
 function addToBag(element) {
   if ($(element).hasClass('mdc-button--active')) {
     $(element).removeClass('mdc-button--active');
+    $(element).attr('title', 'Agregar a mochila');
   } else {
     $(element).addClass('mdc-button--active');
+    $(element).attr('title', 'Quitar de mochila');
   }
-  console.log('add to bag');
 }
 
 function toggleView(element) {
@@ -52,90 +144,26 @@ function roleSelection(e) {
   $('body').attr('data-role', role);
 }
 
-$(function() {
-  const drawer = $('.mdc-drawer');
-  const header = $('.mdc-header');
-  const appBar = $('.mdc-top-app-bar');
-
-  if (drawer) {
-    $(window).scroll(function() {
-      onScroll();
-    });
-
-    onScroll();
+let dragTarget;
+function onDrag(e) {
+  if (!dragTarget) {
+    console.log('Warning! No drag target.');
+    return;
   }
+  dragTarget.css('left', `${ dragTarget.position().left + e.originalEvent.movementX }px`);
+  dragTarget.css('top', `${ dragTarget.position().top + e.originalEvent.movementY }px`);
+}
 
-  $('.user-roles-wrapper input').change(roleSelection);
-  roleSelection({ currentTarget: $('.user-roles-wrapper input[checked]') });
+function onLetGo() {
+  dragTarget.removeClass('dragging');
+  dragTarget = undefined;
+  $(document).off('mousemove', onDrag);
+  $(document).off('mouseup', onLetGo);
+}
 
-  //smoothscroll
-  $('.mdc-drawer a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(window).off("scroll");
-    
-    $('.mdc-list-item').removeClass('mdc-list-item--activated');
-    $(this).addClass('mdc-list-item--activated');
-  
-    var target = this.hash;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top-appBar.height() - 12
-    }, 500, 'swing', function () {
-      $(window).scroll("scroll", onScroll);
-      //window.location.hash = target;
-    });
-  });
-
-  $('.cards--selectable .mdc-card').click(function(e) {
-    let card = $(e.currentTarget);
-    console.log(card.closest('.cards--selectable'));
-    card.closest('.cards--selectable').find('.mdc-card').removeClass('mdc-card--selected');
-    setoggleCardSelection(card);
-  });
-
-  function onScroll() {
-    fixed();
-    updateDrawer();
-  }
-
-  function updateDrawer() {
-    const scrollTop = $(this).scrollTop();
-    
-    let sections = $('.main-wrapper section');
-    let sectionId;
-
-    let threshold = header.height();
-
-    if (scrollTop < threshold) {
-      return;
-    }
-
-    sections.each(function(i, section) {
-      section = $(section);
-      if (scrollTop > section.position().top && section.position().top + section.height() - scrollTop >= 0) {
-        sectionId = section.attr('id');
-      } 
-    });
-
-    $('.mdc-list-item').each(function(i, item) {
-      if ($(item).attr('data-section') == sectionId) {
-        $(item).addClass('mdc-list-item--activated');
-      } else {
-        $(item).removeClass('mdc-list-item--activated');
-      }
-    });
-  }
-
-  function fixed() {
-    const scrollTop = $(this).scrollTop();
-    if (header.height() > scrollTop) {
-      drawer.removeClass('fixed');
-      drawer.css('top', `${ header.height() }px`);
-    } else {
-      drawer.addClass('fixed');
-      drawer.css('top', `${ appBar.height() }px`);
-    }
-  };
-
-  $('.collapse-link').click(collapse);
-});
+function onGrab(e) {
+  dragTarget = $(e.currentTarget);
+  dragTarget.addClass('dragging');
+  $(document).on('mousemove', onDrag);
+  $(document).on('mouseup', onLetGo);
+}
